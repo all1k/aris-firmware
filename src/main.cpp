805f527fa93c7866127aeb67564b1b369e56d8b2
Wahyu Mahardika <wmhrdk@gmail.com>
2022-10-05 12:30:06 +0800

    Added main source file: - src/main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#define TINY_GSM_MODEM_SIM800
+#define TINY_GSM_USE_GPRS true
+#define TINY_GSM_USE_WIFI false
+
+#include <Arduino.h>
+#include <TinyGSM.h>
+#include <SEN0161.h>
+
+#include "configuration/conf.h"
+
+void setup() {
+}
+
+void loop() {
+}
