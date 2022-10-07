8d9bf0e6651cb5d09a968106b0986e3ca38eec5d
Wahyu Mahardika <wmhrdk@gmail.com>
2022-10-07 22:49:12 +0800

    Included sensor libraries - pH sensor lib - Turbidity sensor lib - DO sensor lib

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,14 @@
 #define TINY_GSM_USE_GPRS true
 #define TINY_GSM_USE_WIFI false
 
+#include "configuration/conf.h"
+
 #include <Arduino.h>
 #include <TinyGSM.h>
 #include <sensor.h>
-
-#include "configuration/conf.h"
+#include <ph_sensor.hpp>
+#include <turbidity_sensor.hpp>
+#include <do_sensor.hpp>
 
 void setup() {
 }
