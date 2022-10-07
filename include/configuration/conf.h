14c7e6bd5870d2748287cf026dbed2ef30769520
Wahyu Mahardika <wmhrdk@gmail.com>
2022-10-07 22:21:01 +0800

    Updated configuration files - Added ESP32 macros - Added pH sensor macros - Added DO sensor macros - Added turbidity sensor macros

--- include/configuration/conf.h
+++ include/configuration/conf.h
@@ -1,13 +1,26 @@
-#ifndef ARIS_CONF_H
-#define ARIS_CONF_H
+#ifndef ARIS_CONFIG_H
+#define ARIS_CONFIG_H
 
+/////////////////////////
+// ESP32 CONFIGURATION //
+/////////////////////////
+
+#define ADC_RESOLUTION			4096	// 12-bit analog to digital converter resolution
+#define VREF					3.3f	// 3.3V voltage reference
 
 ////////////////////////////
 // ANALOG pH METER MACROS //
 ////////////////////////////
 
-#define SEN0161_PIN							A0
-#define SEN0161_OFFSET						0.00
+#define PH_SENSOR_PIN			A0		// pH sensor pin number
+#define PH_SENSOR_OFFSET		0.00	// pH value offset
 
+////////////////////////////////////
+// DISSOLVED OXYGEN SENSOR MACROS //
+////////////////////////////////////
+
+#define TWO_POINT_CALIBRATION	false	// Enable or disable two point calibration
+#define SATURATED_DO_VOLTAGE	1.6f	// Calibratied saturated do voltage
+#define CALIBRATION_TEMPERATURE	25.0f	// Temperature while calibration performed
 
 #endif
