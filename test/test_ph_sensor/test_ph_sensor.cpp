cad3192569c54c2269defae6fb6c1a99107628a0
Wahyu Mahardika <wmhrdk@gmail.com>
2022-10-07 15:38:54 +0800

    Added test failed conditon - test_ph_sensor - test_turbidity_sensor

--- test/test_ph_sensor/test_ph_sensor.cpp
+++ test/test_ph_sensor/test_ph_sensor.cpp
@@ -23,11 +23,17 @@ void tearDown(void) {
 
 void test_voltage_reading(void) {
 	float voltage = sensor->getVoltage();
+	if (voltage < 0.0f) {
+		TEST_FAIL_MESSAGE("-1.0 Voltage returned");
+	}
 	TEST_ASSERT_FLOAT_WITHIN(1.0f, 0.0f, voltage);
 }
 
 void test_data_reading(void) {
 	float data = sensor->getData();
+	if (data < 0.0f) {
+		TEST_FAIL_MESSAGE("-1.0 Data returned");
+	}
 	TEST_ASSERT_FLOAT_WITHIN(1.0f, 0.0f, data);
 }
 
