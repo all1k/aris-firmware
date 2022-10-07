6d85570f908b84508344db008170a3533fd98baa
Wahyu Mahardika <wmhrdk@gmail.com>
2022-10-07 22:40:14 +0800

    Implemented turbidity sensor calculations

--- lib/turbidity_sensor/src/turbidity_sensor.cpp
+++ lib/turbidity_sensor/src/turbidity_sensor.cpp
@@ -15,9 +15,10 @@ bool TurbiditySensor::init(void) {
 
 bool TurbiditySensor::update(void) {
 	voltage_ = getVoltage();
-	data_ = (voltage_ * 14.0f) / VREF;
-
-	if ((data_ > 14.0f) || (data_ < 0.0f)) {
+	voltage_ = (voltage_ * 5.0f) / VREF;
+	float A[3] = {-1120.4f, 5742.3f, 4352.9f};
+	data_ = ((A[0]*std::pow(voltage_, 2)) + (A[1]*voltage_)) - A[2];
+	if (data_ < 0.0f) {
 		return false;
 	}
 	else {
