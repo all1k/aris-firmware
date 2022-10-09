c981b53c4cc3bbf28a15f02525c7b6002a453fee
Wahyu Mahardika <wmhrdk@gmail.com>
2022-10-09 14:03:27 +0800

    Added temperature DS18B20 sensor library - temperature_sensor.hpp - temperature_sensor.cpp

---
+++ lib/temperature_sensor/src/temperature_sensor.cpp
@@ -0,0 +1,53 @@
+#include "temperature_sensor.hpp"
+
+namespace arislib {
+
+TemperatureSensor::TemperatureSensor(std::uint8_t pin) {
+	pin_ = pin;
+}
+
+bool TemperatureSensor::init(void) {
+	voltage_ = 0.0f;
+	data_ = 0.0f;
+
+	wire_.reset(new OneWire(pin_));
+
+	return true;
+}
+
+bool TemperatureSensor::update(void) {
+	std::uint8_t data[12], addr[8];
+	if (!wire_->search(addr)) {
+		wire_->reset_search();
+		return false;
+	}
+	else if (OneWire::crc8(addr, 7) != addr[7]) {
+		return false;
+	}
+	else if ((addr[0]!=0x10) && (addr[0]!=0x28)) {
+		return false;
+	}
+
+	wire_->reset();
+	wire_->select(addr);
+	wire_->write(0x44, 1);
+
+	std::uint8_t present = wire_->reset();
+
+	wire_->select(addr);
+	wire_->write(0xBE);
+	for (int i = 0; i < 9; i++) {
+		data[i] = wire_->read();
+	}
+
+	wire_->reset_search();
+
+	std::uint8_t high_byte = data[1];
+	std::uint8_t low_byte = data[0];
+	float temp_read = ((high_byte << 8) | low_byte);
+	data_ = temp_read / 16;
+
+	return true;
+}
+
+}
---
+++ lib/temperature_sensor/src/temperature_sensor.hpp
@@ -0,0 +1,27 @@
+#ifndef TEMPERATURE_SENSOR_HPP
+#define TEMPERATURE_SENSOR_HPP
+
+#include <memory>
+#include <Arduino.h>
+#include <OneWire.h>
+#include <sensor.hpp>
+
+namespace arislib {
+
+class TemperatureSensor : public Sensor<float> {
+	private:
+		std::unique_ptr<OneWire> wire_;
+
+	public:
+		TemperatureSensor(std::uint8_t pin);
+		bool init(void);
+		bool update(void);
+
+		std::uint16_t getAdcValue(void) = delete;
+		float getVoltage(void) = delete;
+};
+
+
+}
+
+#endif
