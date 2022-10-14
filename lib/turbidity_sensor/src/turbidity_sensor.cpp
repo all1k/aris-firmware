3af6801da4b298daa248686ec6133696dcf6f2df
Wahyu Mahardika <wmhrdk@gmail.com>
2022-10-14 19:06:57 +0800

    Added turbidity sensor VREF macro

--- lib/turbidity_sensor/src/turbidity_sensor.cpp
+++ lib/turbidity_sensor/src/turbidity_sensor.cpp
@@ -14,8 +14,7 @@ bool TurbiditySensor::init(void) {
 }
 
 bool TurbiditySensor::update(void) {
-	voltage_ = getVoltage();
-	voltage_ = (voltage_ * 5.0f) / VREF;
+	voltage_ = (getVoltage() * TURBIDITY_SENSOR_VREF) / VREF;
 	float A[3] = {-1120.4f, 5742.3f, 4352.9f};
 	data_ = ((A[0]*std::pow(voltage_, 2)) + (A[1]*voltage_)) - A[2];
 	if (data_ < 0.0f) {
