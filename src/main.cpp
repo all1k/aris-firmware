b0a85752d992e396534ca79b7bada13666136dd4
Wahyu Mahardika <wmhrdk@gmail.com>
2022-10-19 22:44:13 +0800

    Updated main include header

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <memory>
 #include <Arduino.h>
 #include <TinyGSM.h>
-#include <sensor.h>
+#include <sensor.hpp>
 #include <ph_sensor.hpp>
 #include <do_sensor.hpp>
 #include <turbidity_sensor.hpp>
