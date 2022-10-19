62c8db030ffdb7668c235f88e526af6be7f397d7
Wahyu Mahardika <wmhrdk@gmail.com>
2022-10-19 22:33:05 +0800

    Added sensor manager library

---
+++ lib/sensor_manager/src/sensor_manager.hpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+namespace aris {
+
+class SensorManager {
+	private:
+		std::map<std::string, std::shared_ptr<Sensor>> sensors_;
+
+	public:
+		bool run(float frequency);
+		bool spawn(const std::string& name, const std::shared_ptr<Sensor>& ptr);
+		float getData(const std::string& name);
+}
+
+}
