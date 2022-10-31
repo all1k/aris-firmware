e34b04acfa6b8b1499ab81b84b750402b81d5328
Wahyu Mahardika <wmhrdk@gmail.com>
2022-10-31 23:59:05 +0800

    Added test_utilities.h

---
+++ test/test_utilities.h
@@ -0,0 +1,76 @@
+#ifndef _ARIS_TEST_UTILITIES_HPP_
+#define _ARIS_TEST_UTILITIES_HPP_
+
+#ifdef TEMPERATURE_SENSOR_TEST
+#define QUANTITY	"Temperature"
+#define UNIT		"Degree Celcius"
+#elif defined TURBIDITY_SENSOR_TEST
+#define QUANTITY	"Turbidity"
+#define UNIT		"NTU"
+#elif defined PH_SENSOR_TEST
+#define QUANTITY	"pH"
+#define UNIT		""
+#elif defined EC_SENSOR_TEST
+#define QUANTITY	"Conductivity"
+#define UNIT		"ms/cm"
+#elif defined DO_SENSOR_TEST
+#define QUANTITY	"Dissolved Oxygen"
+#define UNIT		"mg/L"
+#endif
+
+#include <unity.h>
+#include <vector>
+#include <numeric>
+#include <string>
+
+namespace aristest {
+
+void logValue(float value) {
+	std::string msg;
+	msg.append(QUANTITY);
+	msg.append(" = ");
+	msg.append(std::to_string(value));
+	msg.append(" ");
+	msg.append(UNIT);
+	TEST_MESSAGE(msg.c_str());
+}
+
+void logValue(float value, const std::string& quantity, const std::string& unit) {
+	std::string msg;
+	msg.append(quantity);
+	msg.append(" = ");
+	msg.append(std::to_string(value));
+	msg.append(" ");
+	msg.append(unit);
+	TEST_MESSAGE(msg.c_str());
+}
+
+template<typename Type>
+float getAverage(std::vector<Type> const& v) {
+	if (v.empty()) {
+		return 0;
+	}
+	else {
+		return std::accumulate(v.begin(), v.end(), 0.0) / v.size();
+	}
+}
+
+template<typename Type>
+void checkDataValidity(Type data, Type upper_tresh, Type low_tresh) {
+	if (data < low_tresh) {
+		logValue(data);
+		TEST_FAIL_MESSAGE("Data below lower threshold");
+	}
+	else if (data > upper_tresh) {
+		logValue(data);
+		TEST_FAIL_MESSAGE("Data above upper threshold");
+	}
+	else {
+		logValue(data);
+		TEST_PASS();
+	}
+}
+
+}
+
+#endif
